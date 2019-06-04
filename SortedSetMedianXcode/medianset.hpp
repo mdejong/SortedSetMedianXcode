d8732878662734d14aa13eeb1101c951875c0574
Mo DeJong <mo@modejong.com>
2019-06-03 19:19:31 -0700

    add source code for multiset impl

---
+++ SortedSetMedianXcode/medianset.hpp
@@ -0,0 +1,177 @@
+//
+//  medianset.hpp
+//
+//  Created by Mo DeJong on 5/31/19.
+//
+//  multiset that maintains an iterator to the middle
+//  value in the set. This logic handles inserting
+//  and element in logn time, deleting an element in
+//  logn time, and checking the value of the median
+//  element in o1 time via use of the iterator.
+
+#ifndef medianset_hpp
+#define medianset_hpp
+
+#include <vector>
+#include <string>
+#include <set>
+#include <sstream>
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class medianset {
+protected:
+  multiset<T> values;
+  typename multiset<T>::iterator median;
+  
+public:
+  medianset() : values(), median(values.begin()) {
+  };
+  
+  size_t size() const {
+    return values.size();
+  }
+  
+  void insert(T val) {
+    size_t s = size();
+    
+    if (s == 0) {
+      // Special case for empty set, insert the value and update
+      // the median iterator to the first value
+      
+      values.insert(val);
+      median = values.begin();
+      return;
+    }
+    
+    T medianVal = *median;
+    
+    bool isPostShift = false;
+    
+    if ((s & 0x1) == 0) {
+      // size is even
+      
+      if (val < medianVal) {
+        // value is to the left of median, nop
+      } else {
+        // if val is equal to or larger than median, shift right
+        isPostShift = true;
+      }
+    } else {
+      // size is odd
+      
+      if (val < medianVal) {
+        // value is to the left, shift left
+        median--;
+      } else {
+        // if val is larger or equal to median, nop
+      }
+    }
+    
+    values.insert(val);
+#if defined(DEBUG)
+    assert(size() == (s+1));
+#endif // DEBUG
+    
+    if (isPostShift) {
+      median++;
+    }
+  }
+  
+  void remove(T val) {
+    // If remove is invoked when there are no elements in the
+    // set or with a val that is not in the set then nop.
+    
+    size_t s = size();
+    
+    auto removeIter = values.find(val);
+    
+    if (s == 0) {
+      return;
+    } else if (removeIter == values.end()) {
+      return;
+    }
+    
+    T medianVal = *median;
+    
+    if ((s & 0x1) == 0) {
+      // size is even
+      
+      if (val <= medianVal) {
+        // value to remove is to the left of median or it is the median
+        median++;
+      } else {
+        // if val is larger than median then nop
+      }
+    } else {
+      // size is odd
+      
+      if (val <= medianVal) {
+        // value to remove is to the left of median or it is the median, nop
+      } else {
+        // if val is larger than median then move media to the left
+        median--;
+      }
+    }
+    
+    values.erase(removeIter);
+#if defined(DEBUG)
+    assert(size() == (s-1));
+#endif // DEBUG
+  }
+  
+  // Determine the "offset" of the median ref if all the values were
+  // stored in a flat array. For example, when the set contains
+  // [1 2 3] then the medianDistance returns 1. Note that this method
+  // it not fast since it must iterate from the start of the container
+  // up to the median ref value to determine the distance.
+  
+  size_t medianDistance() const {
+    if (size() == 0) {
+      return -1;
+    }
+    
+    return distance(values.begin(), median);
+  }
+
+  // Quickly return the median value in the sorted set of values.
+  // Note that this method invocation is invalid when size() is zero.
+  // With 3 values like [1 2 3] this returns 2.
+  // With 4 values like [1 2 3 4] this returns 2.
+  
+  T medianVal() {
+    return *median;
+  }
+  
+  // When the count is odd like [1 2 3] this method returns [2 2]
+  // but in the even case like [1 2 3 4] this method returns [2 3]
+  
+  pair<T, T> medianPair() const {
+    size_t s = size();
+    
+    if ((s & 0x1) == 0) {
+      // even
+      auto i2 = median;
+      i2++;
+      return pair<T, T>(*median, *i2);
+    } else {
+      // odd
+      T val = *median;
+      return pair<T, T>(val, val);
+    }
+  }
+  
+  typename multiset<T>::iterator begin() const {
+    return values.begin();
+  }
+
+  typename multiset<T>::iterator end() const {
+    return values.end();
+  }
+  
+};
+
+#endif // medianset_hpp
---
+++ SortedSetMedianXcode/medianset_main.h
@@ -0,0 +1,215 @@
+//
+//  medianset_main.h
+//  SortedSetMedianXcode
+//
+//  Created by Mo DeJong on 5/31/19.
+//  Copyright © 2019 HelpURock. All rights reserved.
+//
+//  Impl of programming challenge with C++ multiset.
+//  This logic handles insert and delete but iteration
+//  over all the elements in the window results in slow
+//  runtime since every value depends on looping over
+//  every element in the container.
+
+#ifndef medianset_main_h
+#define medianset_main_h
+
+//#include <bits/stdc++.h>
+
+#include <vector>
+#include <string>
+#include <set>
+#include <sstream>
+
+#include <iostream>
+
+#include "medianset.hpp"
+
+using namespace std;
+
+static
+vector<string> split_string(string);
+
+// Determine median value given a set of int values
+// 1. Even # elems, return ave of middle values
+// 2. Odd # elems, return middle value
+
+static inline
+float median(const medianset<int> & values) {
+  int isEven = (int) ((values.size() % 2) == 0);
+  pair<int, int> p = values.medianPair();
+  
+  if (isEven) {
+    // even
+    int v1 = p.first;
+    int v2 = p.second;
+    return (v1 + v2) / 2.0f;
+  } else {
+    // odd
+    return float(p.first);
+  }
+}
+
+// return an integer representing the number of client notifications
+// when a window of size d is used over expenditure values.
+
+static inline
+int activityNotifications(vector<int> expenditure, int d) {
+  int numWindows = (int) expenditure.size() - d;
+  int numNotifications = 0;
+  
+  // Init multiset which contains a balanced tree of int values,
+  // note that duplicates can exist so use multiset instead of set.
+  
+  medianset<int> window;
+  int windowRemoveIndex = 0;
+  int windowInsertIndex = d;
+  
+  for ( int i = 0; i < d; i++ ) {
+    int v = expenditure[i];
+    window.insert(v);
+  }
+#if defined(DEBUG)
+  assert(window.size() == d);
+#endif // DEBUG
+  
+  // window of values configured, grab today value,
+  // run calculation logic, then update sorted set.
+  
+  for ( int i = 0; i < numWindows; i++ ) {
+    // Execute median logic
+    
+    int expenditureToday = expenditure[windowInsertIndex];
+    
+    /*
+     cout << "expenditureToday " << expenditureToday << endl;
+     cout << "window ";
+     for ( auto it = window.begin(); it != window.end(); it++ ) {
+     cout << *it << " ";
+     }
+     cout << endl;
+     */
+
+    if (1) {
+      cout << "expenditureToday " << expenditureToday << endl;
+      int medianInd = (int) window.medianDistance();
+      cout << "median " << window.medianVal() << " is at offset " << medianInd << endl;
+      
+      cout << "window ";
+      for ( auto it = window.begin(); it != window.end(); it++ ) {
+        cout << *it << " ";
+      }
+      cout << endl;
+    }
+    
+    // Increment num notifications if a notification was delivered
+    // over this window as indicated by the mean
+    
+    float dMedian = 2 * median(window);
+
+    if (float(expenditureToday) >= dMedian) {
+      numNotifications += 1;
+    }
+    
+    // Update container
+    
+    int expenditureToRemove = expenditure[windowRemoveIndex];
+    
+    if (expenditureToRemove == expenditureToday) {
+      // nop for case of removing and then inserting the same value
+    } else {
+      // the iterator should stay on the same index, so determine
+      // if it just moved to the left or the right and move to
+      // the left or right to account for the change.
+      
+#if defined(DEBUG)
+      int medianInd = (int) window.medianDistance();
+#endif // DEBUG
+      
+      cout << "expenditureToRemove " << expenditureToRemove << endl;
+      
+      window.remove(expenditureToRemove);
+      window.insert(expenditureToday);
+      
+#if defined(DEBUG)
+      int medianIndAfter = (int) window.medianDistance();
+      assert(medianInd == medianIndAfter);
+#endif // DEBUG
+    }
+    
+    windowInsertIndex += 1;
+    windowRemoveIndex += 1;
+  }
+  
+  return numNotifications;
+}
+
+// Execute main logic using input inStr and returning the output
+
+static inline
+string mset_main(const string & inStr)
+{
+  istringstream inStream;
+  inStream.str(inStr);
+  
+  ostringstream outStream;
+  
+  string nd_temp;
+  getline(inStream, nd_temp);
+  
+  vector<string> nd = split_string(nd_temp);
+  
+  int n = stoi(nd[0]);
+  int d = stoi(nd[1]);
+  
+  string expenditure_temp_temp;
+  getline(inStream, expenditure_temp_temp);
+  
+  vector<string> expenditure_temp = split_string(expenditure_temp_temp);
+  
+  vector<int> expenditure(n);
+  
+  for (int i = 0; i < n; i++) {
+    int expenditure_item = stoi(expenditure_temp[i]);
+    
+    expenditure[i] = expenditure_item;
+  }
+  
+  int result = activityNotifications(expenditure, d);
+  
+  outStream << result << "\n";
+  
+  return outStream.str();
+}
+
+static inline
+vector<string> split_string(string input_string) {
+  string::iterator new_end = unique(input_string.begin(), input_string.end(), [] (const char &x, const char &y) {
+    return x == y and x == ' ';
+  });
+  
+  input_string.erase(new_end, input_string.end());
+  
+  while (input_string[input_string.length() - 1] == ' ') {
+    input_string.pop_back();
+  }
+  
+  vector<string> splits;
+  char delimiter = ' ';
+  
+  size_t i = 0;
+  size_t pos = input_string.find(delimiter);
+  
+  while (pos != string::npos) {
+    splits.push_back(input_string.substr(i, pos - i));
+    
+    i = pos + 1;
+    pos = input_string.find(delimiter, i);
+  }
+  
+  splits.push_back(input_string.substr(i, min(pos, input_string.length()) - i + 1));
+  
+  return splits;
+}
+
+#endif // medianset_main
